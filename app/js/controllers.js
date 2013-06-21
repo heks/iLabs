8e45f56904a1449cca90b78480858e5724b999c9
Rasmi Roy <rasmi.roy@northwestern.edu>
2013-06-21 09:17:34 -0500

    updated the files to implement the graph to manipulate dynamic data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -188,6 +188,250 @@ function appController($scope, $location){
         $scope.drawGraph('g2', t);
         
       }
+
+      $scope.Graph = function(){
+        var cid = $("#coupon_Id").text();
+        var ckey = $("#coupon_Key").text();
+        var expid = $('#exp_Id').text();
+
+        var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', 'http://ilabs.sesp.northwestern.edu/iLabServiceBroker/ilabServiceBroker.asmx', true);
+
+
+    // Build SOAP request
+    var data = '<?xml version="1.0" encoding="utf-8"?>' +
+                  '<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">'+
+            '<soap12:Header>' +
+            '<sbAuthHeader xmlns="http://ilab.mit.edu">' +
+              '<couponID>'+cid+'</couponID>' +
+              '<couponPassKey>'+ckey+'</couponPassKey>' +
+            '</sbAuthHeader>' +
+          '</soap12:Header>' +
+          '<soap12:Body>' +
+            '<RetrieveResult xmlns="http://ilab.mit.edu">' +
+              '<experimentID>'+expid+'</experimentID>' +
+            '</RetrieveResult>' +
+          '</soap12:Body>' +
+        '</soap12:Envelope>'
+//alert(data);
+    xmlhttp.onreadystatechange = function(){
+      if(xmlhttp.readyState == 4){
+
+        if(xmlhttp.status == 200){
+          xmlDoc = xmlhttp.responseXML;
+          //alert(xmlhttp.responseText);
+          //var response = xmlToJson(xmlDoc);
+          result = $(xmlDoc).find("experimentResults").text();
+          var distance = $(result).find("dataVector")[0].getAttribute('distance');
+          vector1 = $(result).find("dataVector").length;
+          vecdata = $(result).find("dataVector").text();
+          var d1 = $(result).find("dataVector")[2];
+          var d2 = d1.childNodes[0];
+          var d3 = d2.nodeValue;
+          //alert(vector1);
+          //txt = $(result).find("dataVector")[0].getAttributeNode('distance');
+          //var arr = array();
+          var dist_arr = new Array();
+          for(var k=0; k<vector1; k++){
+            var distance = $(result).find("dataVector")[k].getAttribute('distance');
+            dist_arr[k] = distance;
+          }
+          //alert(dist_arr);
+
+          $("#graph").highcharts({
+                       chart: {
+                          type: 'line',
+                          backgroundColor: '#FCFFC5'
+                        },
+                        title: {
+                          text: "radioactivity"
+                        },
+                        
+                        xAxis: {
+                          //categories: ['45','65','75'],
+                          categories: dist_arr,
+                          title: {
+                            text: 'Distances( mm)'
+                          }
+                        },
+                        yAxis: {
+                          title: {
+                            text: 'Radioactivity Intensity( Particle Counts)'
+                          }
+                        },
+                        series: []
+                         
+
+                      });
+var value = new Array(); var tr1 = new Array(); var tr2 = new Array(); var data = new Array();var tr3 = new Array();var tr4 = new Array();
+var tr5 = new Array();var tr6 = new Array();var tr7 = new Array();var tr8 = new Array();var tr9 = new Array();var tr10 = new Array();
+                        var chart = $('#graph').highcharts();
+                          for(var i=0; i < vector1; i++){
+                                  var distance = $(result).find("dataVector")[i].getAttribute('distance');
+                                  var d1 = $(result).find("dataVector")[i];
+                                  var d2 = d1.childNodes[0];
+                                  var d3 = d2.nodeValue;
+                                  value[i] = d3;
+                                  var split = value[i].split(",");
+                                  var length = split.length;
+                                  
+                                  switch(length){
+                                    case 1:
+                                            tr1[i] = parseInt(split[0]);
+                                            break;
+                                    case 2:
+                                            tr1[i] = parseInt(split[0]);tr2[i] = parseInt(split[1]);
+                                            break;
+                                    case 3:
+                                            tr1[i] = parseInt(split[0]);tr2[i] = parseInt(split[1]);tr3[i] = parseInt(split[2]);
+                                            break;
+                                    case 4:
+                                            tr1[i] = parseInt(split[0]);tr2[i] = parseInt(split[1]);tr3[i] = parseInt(split[2]);
+                                            tr4[i] = parseInt(split[3]);
+                                            break;
+                                    case 5:
+                                            tr1[i] = parseInt(split[0]);tr2[i] = parseInt(split[1]);tr3[i] = parseInt(split[2]);
+                                            tr4[i] = parseInt(split[3]);tr5[i] = parseInt(split[4]);
+                                            break;
+                                    case 6:
+                                            tr1[i] = parseInt(split[0]);tr2[i] = parseInt(split[1]);tr3[i] = parseInt(split[2]);
+                                            tr4[i] = parseInt(split[3]);tr5[i] = parseInt(split[4]);tr6[i] = parseInt(split[5]);
+                                            break;
+                                    case 7:
+                                            tr1[i] = parseInt(split[0]);tr2[i] = parseInt(split[1]);tr3[i] = parseInt(split[2]);
+                                            tr4[i] = parseInt(split[3]);tr5[i] = parseInt(split[4]);tr6[i] = parseInt(split[5]);
+                                            tr7[i] = parseInt(split[6]);
+                                            break;
+                                    case 8:
+                                            tr1[i] = parseInt(split[0]);tr2[i] = parseInt(split[1]);tr3[i] = parseInt(split[2]);
+                                            tr4[i] = parseInt(split[3]);tr5[i] = parseInt(split[4]);tr6[i] = parseInt(split[5]);
+                                            tr7[i] = parseInt(split[6]);tr8[i] = parseInt(split[7]);
+                                            break;
+                                    case 9:
+                                            tr1[i] = parseInt(split[0]);tr2[i] = parseInt(split[1]);tr3[i] = parseInt(split[2]);
+                                            tr4[i] = parseInt(split[3]);tr5[i] = parseInt(split[4]);tr6[i] = parseInt(split[5]);
+                                            tr7[i] = parseInt(split[6]);tr8[i] = parseInt(split[7]);tr9[i] = parseInt(split[8]);
+                                            break;
+                                    case 10:
+                                            tr1[i] = parseInt(split[0]);tr2[i] = parseInt(split[1]);tr3[i] = parseInt(split[2]);
+                                            tr4[i] = parseInt(split[3]);tr5[i] = parseInt(split[4]);tr6[i] = parseInt(split[5]);
+                                            tr7[i] = parseInt(split[6]);tr8[i] = parseInt(split[7]);tr9[i] = parseInt(split[8]);
+                                            tr10[i] = parseInt(split[9]);
+                                            break;
+
+                                    }
+                                  
+                                }
+                                
+//alert(tr1 +" ; "+ tr2+"="+length);
+var chart = $('#graph').highcharts();
+switch(length){
+  case 1: 
+  var combine = new Array(tr1);
+  break;
+  case 2:
+  var combine = new Array(tr1,tr2);
+  for(var j=0; j<combine.length;j++){
+    chart.addSeries({
+                    name: "Trial"+(j+1),
+                    data: combine[j]
+                    
+                  });
+  }
+  break;
+  case 3:
+  var combine = new Array(tr1,tr2,tr3);
+  for(var j=0; j<combine.length;j++){
+    chart.addSeries({
+                    name: "Trial"+(j+1),
+                    data: combine[j]
+                    
+                  });
+  }
+  break;
+  case 4:
+  var combine = new Array(tr1,tr2,tr3,tr4);
+  for(var j=0; j<combine.length;j++){
+    chart.addSeries({
+                    name: "Trial"+(j+1),
+                    data: combine[j]
+                    
+                  });
+  }
+  break;
+  case 5:
+  var combine = new Array(tr1,tr2,tr3,tr4,tr5);
+  for(var j=0; j<combine.length;j++){
+    chart.addSeries({
+                    name: "Trial"+(j+1),
+                    data: combine[j]
+                    
+                  });
+  }
+  break;
+  case 6:
+  var combine = new Array(tr1,tr2,tr3,tr4,tr5,tr6);
+  for(var j=0; j<combine.length;j++){
+    chart.addSeries({
+                    name: "Trial"+(j+1),
+                    data: combine[j]
+                    
+                  });
+  }
+  break;
+  case 7:
+  var combine = new Array(tr1,tr2,tr3,tr4,tr5,tr6,tr7);
+  for(var j=0; j<combine.length;j++){
+    chart.addSeries({
+                    name: "Trial"+(j+1),
+                    data: combine[j]
+                    
+                  });
+  }
+  break;
+  case 8:
+  var combine = new Array(tr1,tr2,tr3,tr4,tr5,tr6,tr7,tr8);
+  for(var j=0; j<combine.length;j++){
+    chart.addSeries({
+                    name: "Trial"+(j+1),
+                    data: combine[j]
+                    
+                  });
+  }
+  break;
+  case 9:
+  var combine = new Array(tr1,tr2,tr3,tr4,tr5,tr6,tr7,tr8,tr9);
+  for(var j=0; j<combine.length;j++){
+    chart.addSeries({
+                    name: "Trial"+(j+1),
+                    data: combine[j]
+                    
+                  });
+  }
+  break;
+  case 10:
+  var combine = new Array(tr1,tr2,tr3,tr4,tr5,tr6,tr7,tr8,tr9,tr10);
+  for(var j=0; j<combine.length;j++){
+    chart.addSeries({
+                    name: "Trial"+(j+1),
+                    data: combine[j]
+                    
+                  });
+  }
+  break;
+}
+
+        }else{
+           alert("Error: " + xmlhttp.status + ":" + xmlhttp.responseText);
+        }
+      }
+    }
+
+    // Send the POST request
+    xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+    xmlhttp.send(data);
+  
+      };
       
 
       $scope.drawGraph = function(n, t){
