dad7a78c85aab4ab8562a94fb94af5761d3da07c
Rasmi Roy <rasmi.roy@northwestern.edu>
2013-11-01 15:15:45 -0500

    updated the files for #726 and #731 11-01-13(1)

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,45 +1,20 @@
 'use strict';
 
-/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
-
-describe('my app', function() {
+describe('iLab Mobile Application', function() {
 
   beforeEach(function() {
     browser().navigateTo('../../app/index.html');
   });
 
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/view1");
+  it('should automatically redirect to the default path /login', function() {
+    expect(browser().location().url()).toBe("/login");
   });
 
-
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view1');
-    });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
-    });
-
+  it('should perform authentication on login page', function(){
+  	input('loginform.username').enter('student1');
+  	input('loginform.password').enter('student1');
+  	element(':button').click();
+  	expect(browser().location().url()).toBe("/home");
   });
 });
+
